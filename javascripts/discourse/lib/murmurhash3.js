cda64c7de7279d49ef8b8b1f02bbc7c9fc67dd63
Jarek Radosz <jradosz@gmail.com>
2024-03-27 17:52:20 +0100

    DEV: Update linting

--- javascripts/discourse/lib/murmurhash3.js
+++ javascripts/discourse/lib/murmurhash3.js
@@ -1,16 +1,18 @@
+/* eslint-disable */
+// prettier-ignore
 // SPDX-License-Identifier: MIT
 
 /**
  * JS Implementation of MurmurHash3 (r136) (as of May 20, 2011)
- * 
+ *
  * @author <a href="mailto:gary.court@gmail.com">Gary Court</a>
  * @see http://github.com/garycourt/murmurhash-js
  * @author <a href="mailto:aappleby@gmail.com">Austin Appleby</a>
  * @see http://sites.google.com/site/murmurhash/
- * 
+ *
  * @param {string} key ASCII only
  * @param {number} seed Positive integer only
- * @return {number} 32-bit positive integer hash 
+ * @return {number} 32-bit positive integer hash
  */
 
 export default function(key, seed) {
